f3ef4375700b8505297a09e10a141c42ec22ffd9
nirina3040 <nirinaolivier9@gmail.com>
2026-06-04 11:49:28 +0300

    mise à jour de bd

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,10 @@
 const { Pool } = require('pg');
 
-const pool = process.env.NODE_ENV === 'production'
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    })
-  : new Pool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
